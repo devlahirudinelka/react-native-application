b30c9e5d77ad4e1cdafc66192135de73bd4aa57c
Lahiru D K Yapa <lahirudinelka0611@gmail.com>
2023-05-14 14:43:20 +0530

    styles

--- App.tsx
+++ App.tsx
@@ -1,12 +1,46 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello, World</Text>
-      </View>
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={[styles.container, styles.pink]}>
+          <Text style={styles.text}>
+            Hello , World ! Hi
+          </Text>
+        </View>
+      </SafeAreaView>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  pink: {
+    backgroundColor: 'teal',
+  },
+  container: {
+    paddingVertical: 10,
+    // paddingHorizontal: 30,
+    borderColor: 'white',
+    borderRadius: 0,
+    borderWidth: 2,
+    marginVertical: 0,
+    marginHorizontal: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  text: {
+    color: 'white',
+    fontWeight: 900,
+  },
+  safeArea: {
+    flex: 2,
+  },
+});
+
 export default App;
